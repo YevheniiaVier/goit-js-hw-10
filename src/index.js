c1609131dfae90f0b10d1e5af20f391cef279223
YevheniiaVier <gotashk1983@gmail.com>
2022-11-12 20:58:53 +0200

    add final changes

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchCountries } from './fetch-countries.js';
 import { getRefs } from './get-refs.js';
-
 import debounce from 'lodash.debounce';
-const DEBOUNCE_DELAY = 300;
 
-function countryCardTpl(countries) {
-  const { flags, name, capital, population, languages } = countries[0];
+const DEBOUNCE_DELAY = 300;
+const refs = getRefs();
+
+function countryCardTpl([{ flags, name, capital, population, languages }]) {
   const lang = Object.values(languages).join(', ');
   return `<div class='country__main'>
-  <div class="img_thumb"><img src="${flags.svg}" alt="${
-    name.official
-  }" class='country__flag'></div>
-  <h2 class='country__name'>${name.official}</h2>
+  <img src="${flags.svg}" alt="${name.common}" class='country__flag'>
+  <h2 class='country__name'>${name.common}</h2>
 </div>
 <ul class='content'>
   ${
@@ -39,15 +37,13 @@ function countryListMarkup(countryList) {
     .map(({ name, flags }) => {
       return `
     <li class="country__item">
-  <div class="img_thumb"><img src="${flags.svg}" alt="${name.official}" class='country__flag'></div>
-  <h2 class='country__name'>${name.official}</h2>
+ <img src="${flags.svg}" alt="${name.common}" class = 'country__flag'>
+   ${name.common}
 </li>
     `;
     })
     .join('');
 }
-
-const refs = getRefs();
 
 refs.searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
@@ -57,19 +53,29 @@ function onSearch(e) {
     clearContent();
     return;
   }
-
-  fetchCountries(searchQuery).then(renderContent).catch(onFetchError);
+  fetchCountries(searchQuery)
+    .then(body => {
+      body.forEach((element, index) => {
+        if (element.name.common === 'Russia') {
+          body[index].flags.svg =
+            'https://e7.pngegg.com/pngimages/642/765/png-clipart-feces-pile-of-poo-emoji-logo-others-miscellaneous-brown-thumbnail.png';
+        }
+      });
+      renderContent(body);
+    })
+    .catch(onFetchError);
 }
 
 function renderCountryCard(countries) {
+  clearContent();
   const markup = countryCardTpl(countries);
   refs.countryInfo.innerHTML = markup;
 }
 
 function renderCountryList(countries) {
-  //   clearContent();
+  clearContent();
   const markup = countryListMarkup(countries);
-  refs.countryInfo.innerHTML = markup;
+  refs.countryList.innerHTML = markup;
 }
 
 function onFetchError(error) {
@@ -78,6 +84,7 @@ function onFetchError(error) {
 
 function clearContent() {
   refs.countryInfo.innerHTML = '';
+  refs.countryList.innerHTML = '';
 }
 
 function renderContent(countries) {
@@ -85,7 +92,6 @@ function renderContent(countries) {
     clearContent();
     Notify.info('Too many matches found. Please enter a more specific name.');
   } else if (countries.length >= 2 && countries.length < 10) {
-    clearContent();
     renderCountryList(countries);
   } else if (countries.length === 1) {
     renderCountryCard(countries);
